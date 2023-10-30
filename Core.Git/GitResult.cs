22d6ea0c42264afc861e6227da3964865835b100
toddcoder <38821140+toddcoder@users.noreply.github.com>
2023-10-30 12:58:47 -0500

    Add files via upload

---
+++ Core.Git/GitResult.cs
@@ -0,0 +1,10 @@
+﻿namespace Core.Git;
+
+public abstract class GitResult
+{
+   public static GitResult Success => new GitSuccess();
+
+   public static GitResult Error => new GitError();
+
+   public static implicit operator GitResult(string text) => new GitLine(text);
+}
---
+++ Core.Git/GitSuccess.cs
@@ -0,0 +1,5 @@
+﻿namespace Core.Git;
+
+public sealed class GitSuccess : GitResult
+{
+}
