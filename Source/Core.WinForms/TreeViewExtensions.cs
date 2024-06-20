56c5a0ccae5a1a6ba957252c1c417a667f071379
Todd Bennett <toddcoder@comcast.net>
2024-06-20 09:47:38 -0500

    TreeView extensions

---
+++ Source/Core.WinForms/TreeViewExtensions.cs
@@ -0,0 +1,92 @@
+﻿namespace Core.WinForms;
+
+public static class TreeViewExtensions
+{
+   public static IEnumerable<TreeNode> AllNodes(this TreeView treeView, bool includeChildren)
+   {
+      if (includeChildren)
+      {
+         foreach (TreeNode node in treeView.Nodes)
+         {
+            yield return node;
+
+            foreach (var childNode in childNodes(node))
+            {
+               yield return childNode;
+            }
+         }
+      }
+      else
+      {
+         foreach (TreeNode node in treeView.Nodes)
+         {
+            yield return node;
+         }
+      }
+
+      yield break;
+
+      static IEnumerable<TreeNode> childNodes(TreeNode node)
+      {
+         foreach (TreeNode childNode in node.Nodes)
+         {
+            yield return childNode;
+
+            foreach (var subNode in childNodes(childNode))
+            {
+               yield return subNode;
+            }
+         }
+      }
+   }
+
+   public static IEnumerable<TreeNode> AllNodes(this TreeView treeView, Func<TreeNode, bool> predicate, bool includeChildren)
+   {
+      if (includeChildren)
+      {
+         foreach (TreeNode node in treeView.Nodes)
+         {
+            if (predicate(node))
+            {
+               yield return node;
+            }
+
+            foreach (var childNode in childNodes(node))
+            {
+               yield return childNode;
+            }
+         }
+      }
+      else
+      {
+         foreach (TreeNode node in treeView.Nodes)
+         {
+            if (predicate(node))
+            {
+               yield return node;
+            }
+         }
+      }
+
+      yield break;
+
+      IEnumerable<TreeNode> childNodes(TreeNode node)
+      {
+         foreach (TreeNode childNode in node.Nodes)
+         {
+            if (predicate(childNode))
+            {
+               yield return childNode;
+            }
+
+            foreach (var subNode in childNodes(childNode))
+            {
+               if (predicate(childNode))
+               {
+                  yield return subNode;
+               }
+            }
+         }
+      }
+   }
+}
