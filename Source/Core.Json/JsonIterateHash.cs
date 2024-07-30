e65a9b2fa9a18ea8d6b6b7490a4f34cf0a93e7e0
Todd Bennett <toddcoder@comcast.net>
2024-07-30 10:52:12 -0500

    Added try-catch to single-value json retriever

--- Source/Core.Json/JsonIterateHash.cs
+++ Source/Core.Json/JsonIterateHash.cs
@@ -10,58 +10,65 @@ public abstract class JsonIterateHash<TKey, TValue>(string json, JsonRetrieverOp
 
    public virtual Hash<TKey, TValue> Retrieve(params string[] propertyNames)
    {
-      prefixes = [];
-      propertyNameSet = [.. propertyNames];
-      hash = [];
-
-      var reader = getReader();
-
-      propertyName = "";
-      fullPropertyName = "";
-      isRunning = true;
-
-      while (reader.Read() && isRunning)
+      try
       {
-         switch (reader.TokenType)
+         prefixes = [];
+         propertyNameSet = [.. propertyNames];
+         hash = [];
+
+         var reader = getReader();
+
+         propertyName = "";
+         fullPropertyName = "";
+         isRunning = true;
+
+         while (reader.Read() && isRunning)
          {
-            case JsonTokenType.StartObject when bits[JsonRetrieverOptions.UsesPath]:
-               StartObject();
-               break;
-            case JsonTokenType.StartArray when bits[JsonRetrieverOptions.UsesPath]:
-               StartArray();
-               break;
-            case JsonTokenType.EndObject when bits[JsonRetrieverOptions.UsesPath]:
-               EndObject();
-               break;
-            case JsonTokenType.EndArray when bits[JsonRetrieverOptions.UsesPath]:
-               EndArray();
-               break;
-            case JsonTokenType.PropertyName:
-               setValue(reader);
-               setPropertyName();
-               break;
-            case JsonTokenType.String when keyMatches():
-               setValue(reader);
-               String();
-               break;
-            case JsonTokenType.Number when keyMatches():
-               setNumber(reader);
-               Number();
-               break;
-            case JsonTokenType.False when keyMatches():
-               False();
-               break;
-            case JsonTokenType.True when keyMatches():
-               True();
-               break;
-            case JsonTokenType.Null when keyMatches():
-               Null();
-               break;
+            switch (reader.TokenType)
+            {
+               case JsonTokenType.StartObject when bits[JsonRetrieverOptions.UsesPath]:
+                  StartObject();
+                  break;
+               case JsonTokenType.StartArray when bits[JsonRetrieverOptions.UsesPath]:
+                  StartArray();
+                  break;
+               case JsonTokenType.EndObject when bits[JsonRetrieverOptions.UsesPath]:
+                  EndObject();
+                  break;
+               case JsonTokenType.EndArray when bits[JsonRetrieverOptions.UsesPath]:
+                  EndArray();
+                  break;
+               case JsonTokenType.PropertyName:
+                  setValue(reader);
+                  setPropertyName();
+                  break;
+               case JsonTokenType.String when keyMatches():
+                  setValue(reader);
+                  String();
+                  break;
+               case JsonTokenType.Number when keyMatches():
+                  setNumber(reader);
+                  Number();
+                  break;
+               case JsonTokenType.False when keyMatches():
+                  False();
+                  break;
+               case JsonTokenType.True when keyMatches():
+                  True();
+                  break;
+               case JsonTokenType.Null when keyMatches():
+                  Null();
+                  break;
+            }
+
+            SetRunningFlag();
          }
 
-         SetRunningFlag();
+         return hash;
       }
-
-      return hash;
+      catch
+      {
+         return [];
+      }
    }
 }
--- Source/Core.Json/JsonSingleValue.cs
+++ Source/Core.Json/JsonSingleValue.cs
@@ -28,55 +28,62 @@ public class JsonSingleValue(string json, JsonRetrieverOptions options) : JsonIt
 
    public Optional<string> Retrieve(string targetPropertyName)
    {
-      prefixes = [];
-
-      var reader = getReader();
-
-      propertyName = "";
-      fullPropertyName = "";
-
-      while (reader.Read())
+      try
       {
-         switch (reader.TokenType)
+         prefixes = [];
+
+         var reader = getReader();
+
+         propertyName = "";
+         fullPropertyName = "";
+
+         while (reader.Read())
          {
-            case JsonTokenType.StartObject when bits[JsonRetrieverOptions.UsesPath]:
-               StartObject();
-               break;
-            case JsonTokenType.StartArray when bits[JsonRetrieverOptions.UsesPath]:
-               StartArray();
-               break;
-            case JsonTokenType.EndObject when bits[JsonRetrieverOptions.UsesPath]:
-               EndObject();
-               break;
-            case JsonTokenType.EndArray when bits[JsonRetrieverOptions.UsesPath]:
-               EndArray();
-               break;
-            case JsonTokenType.PropertyName:
-               setValue(reader);
-               setPropertyName();
-               break;
-            case JsonTokenType.String when fullPropertyName == targetPropertyName:
-               setValue(reader);
-               String();
-               break;
-            case JsonTokenType.Number when fullPropertyName == targetPropertyName:
-               setNumber(reader);
-               Number();
-               break;
-            case JsonTokenType.False when fullPropertyName == targetPropertyName:
-               False();
-               break;
-            case JsonTokenType.True when fullPropertyName == targetPropertyName:
-               True();
-               break;
-            case JsonTokenType.Null when fullPropertyName == targetPropertyName:
-               Null();
-               break;
+            switch (reader.TokenType)
+            {
+               case JsonTokenType.StartObject when bits[JsonRetrieverOptions.UsesPath]:
+                  StartObject();
+                  break;
+               case JsonTokenType.StartArray when bits[JsonRetrieverOptions.UsesPath]:
+                  StartArray();
+                  break;
+               case JsonTokenType.EndObject when bits[JsonRetrieverOptions.UsesPath]:
+                  EndObject();
+                  break;
+               case JsonTokenType.EndArray when bits[JsonRetrieverOptions.UsesPath]:
+                  EndArray();
+                  break;
+               case JsonTokenType.PropertyName:
+                  setValue(reader);
+                  setPropertyName();
+                  break;
+               case JsonTokenType.String when fullPropertyName == targetPropertyName:
+                  setValue(reader);
+                  String();
+                  break;
+               case JsonTokenType.Number when fullPropertyName == targetPropertyName:
+                  setNumber(reader);
+                  Number();
+                  break;
+               case JsonTokenType.False when fullPropertyName == targetPropertyName:
+                  False();
+                  break;
+               case JsonTokenType.True when fullPropertyName == targetPropertyName:
+                  True();
+                  break;
+               case JsonTokenType.Null when fullPropertyName == targetPropertyName:
+                  Null();
+                  break;
+            }
+
+            SetRunningFlag();
          }
 
-         SetRunningFlag();
+         return _result;
       }
-
-      return _result;
+      catch (Exception exception)
+      {
+         return exception;
+      }
    }
 }
