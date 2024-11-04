f3f271c3dce57d4177c4e83e2499aa211bd8dfac
Todd Bennett <toddcoder@comcast.net>
2024-11-04 13:23:25 -0600

    One common color for read-only alternates

--- Source/Core.WinForms/Controls/ReadOnlyAlternateWriter.cs
+++ Source/Core.WinForms/Controls/ReadOnlyAlternateWriter.cs
@@ -5,22 +5,14 @@ namespace Core.WinForms.Controls;
 public class ReadOnlyAlternateWriter(UiAction uiAction, string[] alternates, bool autoSizeText, Maybe<int> _floor, Maybe<int> _ceiling,
    bool useEmojis) : AlternateWriter(uiAction, alternates, autoSizeText, _floor, _ceiling, useEmojis)
 {
-   protected override (int penSize, Rectangle textRectangle, Rectangle smallRectangle) splitRectangle(Rectangle rectangle)
-   {
-      var penSize = getPenSize(rectangle);
-      return (penSize, rectangle, Rectangle.Empty);
-   }
-
    protected override void onPaint(Graphics g, int index, Rectangle rectangle, UiActionWriter writer, string alternate)
    {
-      var (_, textRectangle, _) = splitRectangle(rectangle);
-
       writer.Font = getFont(index);
-      writer.Color = GetAlternateForeColor(index);
-      var backColor = GetAlternateBackColor(index);
+      writer.Color = Color.White;
+      var backColor = Color.CadetBlue;
       fillRectangle(g, rectangle, backColor);
 
-      writer.Rectangle = textRectangle;
+      writer.Rectangle = rectangle;
 
       writer.Write(g, alternate, uiAction.Type is UiActionType.NoStatus);
    }
