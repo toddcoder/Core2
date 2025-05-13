e222343d0550052b869c5f2c752cb25c62321771
Todd Bennett <toddcoder@comcast.net>
2025-05-13 15:29:34 -0500

    Added TrustServerCertificate=True for integrated security

--- Source/Core.Data/ConnectionStrings/SQLConnectionString.cs
+++ Source/Core.Data/ConnectionStrings/SQLConnectionString.cs
@@ -15,7 +15,7 @@ public class SqlConnectionString : IConnectionString
       var connectionString = $"Data Source={server};Initial Catalog={database};Application Name={application};";
       if (integratedSecurity)
       {
-         connectionString = $"{connectionString}Integrated Security=SSPI;Trusted_Connection=True";
+         connectionString = $"{connectionString}Integrated Security=SSPI;Trusted_Connection=True;TrustServerCertificate=True;";
       }
 
       if (readOnly)
