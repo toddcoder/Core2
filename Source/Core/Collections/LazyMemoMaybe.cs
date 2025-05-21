85676071a31e6b11c914ceb6f95eff37b6b9b0a9
Todd Bennett <toddcoder@comcast.net>
2025-05-21 13:19:30 -0500

    Added LazyMemo class

---
+++ Source/Core/Collections/LazyMemoMaybe.cs
@@ -0,0 +1,15 @@
+﻿using Core.Monads;
+
+namespace Core.Collections;
+
+public class LazyMemoMaybe<TKey, TValue>(LazyMemo<TKey, TValue> lazyMemo) where TKey : notnull where TValue : notnull
+{
+   public Maybe<TValue> this[TKey key]
+   {
+      get
+      {
+         lazyMemo.ForceValue(key);
+         return lazyMemo.GetHash().Maybe[key];
+      }
+   }
+}
