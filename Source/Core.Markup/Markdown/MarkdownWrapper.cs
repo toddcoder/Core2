b02503c12f8423dd9c5f299573f1d67ef2ad0da3
Todd Bennett <toddcoder@comcast.net>
2025-12-22 14:24:21 -0600

    Added MarkdownWrapper class

---
+++ Source/Core.Markup/Markdown/MarkdownWrapper.cs
@@ -0,0 +1,21 @@
+﻿using Core.Collections;
+
+namespace Core.Markup.Markdown;
+
+public class MarkdownWrapper(string source)
+{
+   protected Memo<string, StringHash> styles = new Memo<string, StringHash>.Function(_ => new StringHash());
+   protected Memo<string, string> variables = new Memo<string, string>.Function(v => v);
+
+   public string this[string variable]
+   {
+      get => variables[variable];
+      set => variables[variable] = value;
+   }
+
+   public string this[string style, string key]
+   {
+      get => styles[style].Maybe[key] | "";
+      set => styles[style][key] = value;
+   }
+}
