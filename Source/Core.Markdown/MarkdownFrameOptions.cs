dd0c885d25ac02e09c255109770e6fcc5151dd1e
Todd Bennett <toddcoder@comcast.net>
2026-03-04 13:16:12 -0600

    Added default implementation of IMarkdownFrameOptions

---
+++ Source/Core.Markdown/MarkdownFrameOptions.cs
@@ -0,0 +1,18 @@
+﻿using Core.Collections;
+
+namespace Core.Markdown;
+
+public struct MarkdownFrameOptions() : IMarkdownFrameOptions
+{
+   public string Source { get; set; } = "";
+
+   public bool Tidy { get; set; } = true;
+
+   public StringHash ScalarReplacements { get; set; } = [];
+
+   public StringHash<Replacements> MultipleReplacements { get; set; } = [];
+
+   public StringSet Included { get; set; } = [];
+
+   public StringHash Variables { get; set; } = [];
+}
