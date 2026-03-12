014d456441497646dbfc4a7a72e76ededbaca227
Todd Bennett <tebennett@eprod.com>
2026-03-12 14:57:40 -0500

    Added EscapeMarkdown() extension

---
+++ Source/Core.Markdown/MarkdownExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Core.Matching;
+using Core.Strings;
+
+namespace Core.Markdown;
+
+public static class MarkdownExtensions
+{
+   private const string REGEX_OPERATOR = @"([\\`*_{}\[\]()#+\-.!>|]); u";
+
+   extension(string text)
+   {
+      public string EscapeMarkdown()
+      {
+         if (text.IsEmpty())
+         {
+            return text;
+         }
+
+         return text.Substitute(REGEX_OPERATOR, "$1");
+      }
+   }
+}
